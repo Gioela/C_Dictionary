9a1aa27912c08cb871fe985d121ed27084acdc07
GRaiano <gioele.raiano@gmail.com>
2022-03-13 23:18:59 +0100

    First added dbl_value structure both core and test

--- core/include/geg_dbl_value.h
+++ core/include/geg_dbl_value.h
@@ -7,7 +7,36 @@
  */
 typedef struct _geg_dbl_value
 {
+    geg_nodes_t nodes;
     size_t value;
-    struct geg_nodes_t *nodes;
 } geg_dbl_value_t;
 
+/*
+ * Initialize new geg_dbl_value_t item
+ *
+ * @param value Value to associate to new geg_dbl_value_t element
+ * 
+ * @return if success returns the pointer to geg_dbl_value_t structure with size_t value; NULL otherwise
+ */
+geg_dbl_value_t *geg_dbl_init_item(const size_t *value);
+
+/*
+ * Add item to the end of the head geg_dbl_value_t list
+ *
+ * @param head Head of the dbl_list where item must be added
+ * @param item Item which want to append at the end of geg_sbl_value_t list
+ * 
+ * @return Return 0 if success -1 otherwise
+ */
+int geg_dbl_append_item(geg_dbl_value_t **head, geg_dbl_value_t *item);
+
+/*
+ * Create new geg_dbl_value_t item with given value and add it to the tail of
+ * the head dbl_list
+ *
+ * @param head Head of the dbl_list where item must be added
+ * @param value Value to associate to the last new element of head dbl_list
+ * 
+ * @return Return 0 if success -1 otherwise
+ */
+int geg_dbl_append_value(geg_dbl_value_t **head, size_t *value);
--- core/include/geg_node.h
+++ core/include/geg_node.h
@@ -21,8 +21,25 @@ typedef struct _geg_nodes
 int _geg_node_init_core(geg_nodes_t **node);
 #define geg_node_init(node) _geg_node_init_core((geg_nodes_t **)&node);
 
+/*
+ * Get the last node of the given head geg_node_t list
+ *
+ * @param head Double pointer which points to the head of the geg_nodes_t node
+ * 
+ * @return If success, returns the tail of the geg_nodes_t element with given head.
+ * Otherwise returns NULL.
+ */
 geg_nodes_t *_geg_node_get_tail(const geg_nodes_t **head);
 #define geg_node_tail(head) _geg_node_get_tail((geg_nodes_t **)&head);
 
+/*
+ * Append the given geg_nodes_t struct at the end of the geg_nodes_t head element
+ *
+ * @param head Head of the geg_nodes_t list elements
+ * @param node Element which want to append at the end of the list of head geg_nodes_t
+ * 
+ * @return In case of success, it returns the pointer to the added geg_nodes_t node,
+ * otherwise it returns NULL.
+ */
 geg_nodes_t *_geg_node_append_core(geg_nodes_t **head, geg_nodes_t *node);
 #define geg_node_append(head, node) _geg_node_append_core((geg_nodes_t **)&head, (geg_nodes_t *)&node);
